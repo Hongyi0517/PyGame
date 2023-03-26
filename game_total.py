90fd47db6132dc3a9ab476bd275ecb3fe980973f
HongYi0517 <a0979260517@icloud.com>
2023-03-26 19:39:55 +0800

    start_page

--- game_total.py
+++ game_total.py
@@ -1,90 +1,99 @@
-import pygame,random,math
+import pygame, random, math, time
 
 class Ball(pygame.sprite.Sprite):
-    dx = 0 #更新後x座標
-    dy = 0 #更新後y座標
-    x = 0 #初始x座標
-    y = 0 #初始y座標
-    
-    def __init__(self,screen,speed,srx,sry,radium,color):
+    dx = 0  #x位移
+    dy = 0  #y位移
+    x = 0  #球x坐標
+    y = 0  #球y坐標
+    direction = 0
+    speed = 0
+ 
+    def __init__(self, sp, srx, sry, radium, color):
         pygame.sprite.Sprite.__init__(self)
-        self.screen = pygame.display.get_surface()
+        self.speed = sp
         self.x = srx
         self.y = sry
-        #球的背景
-        self.image = pygame.Surface((radium*2, radium*2))
+        self.image = pygame.Surface([radium*2, radium*2])
         self.image.fill((255,255,255))
-        #球
         pygame.draw.circle(self.image, color, (radium,radium), radium, 0)
-        #檢測碰撞的區域
         self.rect = self.image.get_rect()
-        self.rect.center = (srx, sry)
-        #反彈角度設定
-        diraction = random.randint(20,70)
-        rad = math.radians(diraction)
-        self.dx = speed * math.cos(rad)
-        self.dy = -speed * math.sin(rad)
-    
-    def update(self): #更新球位置
+        self.rect.center = (srx,sry)  #初始位置
+        self.direction = random.randint(40,70)  #移動角度
+ 
+    def update(self):
+        radian = math.radians(self.direction)
+        self.dx = self.speed * math.cos(radian)  #水平速度
+        self.dy = -self.speed * math.sin(radian)  #垂直速度
+        #計算球新坐標
         self.x += self.dx
         self.y += self.dy
         self.rect.x = self.x
         self.rect.y = self.y
-        self.rect.centerx = self.x  
-        self.rect.centery = self.y
-        if(self.rect.left <= 0 or self.rect.right >= self.screen.get_width()):
-            self.dx *= -1
-        elif(self.rect.top <= 0 or self.rect.bottom >= self.screen.get_height()):
-            self.dy *= -1
+        if(self.rect.left <= 0 or self.rect.right >= screen.get_width()-10):  #到達左右邊界
+            self.bouncelr()
+        elif(self.rect.top <= 10):  #到達上邊界
+            self.rect.top = 10
+            self.bounceup()
+        if(self.rect.bottom >= screen.get_height()-10):  #到達下邊界出界
+            return True
+        else:
+            return False
+ 
+    def bounceup(self):  #上邊界反彈
+        self.direction = 360 - self.direction
 
-    def collidebounce(self): #撞到左或右邊界
-        self.dx *= -1
-        
-class Brick(pygame.sprite.Sprite):
+    def bouncelr(self):  #左右邊界反彈
+        self.direction = (180 - self.direction) % 360
+            
+class Brick(pygame.sprite.Sprite):  #磚塊角色
     def __init__(self, color, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([38, 15])
+        self.image = pygame.Surface([38, 13])  #磚塊38x13
         self.image.fill(color)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        
+
 class Pad(pygame.sprite.Sprite):
-    def __init__(self): #板子初始設定
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("C:/板子圖.png")
+        self.image = pygame.image.load("board.png")
         self.image.convert()
         self.rect = self.image.get_rect()
-        self.rect.x = int((screen.get_width() - self.rect.width)/2)
+        self.rect.x = int((screen.get_width() - self.rect.width)/2)  #滑板位置
         self.rect.y = screen.get_height() - self.rect.height - 20
-        self.screen = pygame.display.get_surface()
-    
-    def update(self): #滑鼠操控板子
-        pos = pygame.mouse.get_pos()
-        self.rect.x = pos[0]
-        if self.rect.x > screen.get_width() - self.rect.width:
+ 
+    def update(self):  #滑板位置隨滑鼠移動
+        pos = pygame.mouse.get_pos()  #取得滑鼠坐標
+        self.rect.x = pos[0]  #滑鼠x坐標
+        if self.rect.x > screen.get_width() - self.rect.width:  #不要移出右邊界
             self.rect.x = screen.get_width() - self.rect.width
 
-pygame.init() # 啟動遊戲
-screen = pygame.display.set_mode((600,640)) # 視窗大小
+def gameover(message):  #結束程式
+    global running            
+    text = font1.render(message, 1, (0,0,0))  #顯示訊息
+    screen.blit(text, (screen.get_width()/2-40,screen.get_height()/2-20))
+    pygame.display.update()  #更新畫面
+    time.sleep(3)  #暫停3秒
+    running = False  #結束程式
+
+pygame.init()
+font = pygame.font.Font("C:/mingliu.ttc", 20)  #下方訊息字體
+font1 = pygame.font.Font("C:/mingliu.ttc", 32)  #結束程式訊息字體
+screen = pygame.display.set_mode((600, 400))
 pygame.display.set_caption("打磚塊遊戲")
-#背景設定
 background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill((255,255,255))
-#遊戲起始畫面設定
-font = pygame.font.Font("C:/mingliu.ttc", 20)
-font1 = pygame.font.Font("C:/mingliu.ttc", 32)
-allsprite = pygame.sprite.Group() #群組 
-ball_1 = Ball(screen, 10, 100, 100, 15, (0, 0, 0)) # 設定球
-allsprite.add(ball_1)
-bricks = pygame.sprite.Group() #設定bricks
-allsprite.add(bricks)
-pad = Pad()  #設定pad
-allsprite.add(pad)  
+allsprite = pygame.sprite.Group()  #建立全部角色群組
+bricks = pygame.sprite.Group()  #建立磚塊角色群組
+ball = Ball(15, 300, 350, 10, (0,0,0))  #建立黑色球物件
+allsprite.add(ball)
+pad = Pad()
+allsprite.add(pad)
 clock = pygame.time.Clock()
-for row in range(0, 4): #行數
-    for column in range(0, 15): #列數
+for row in range(0, 4):  #行數
+    for column in range(0, 15):  #列數
         if row == 0:
             brick = Brick((0,0,139), column * 40 + 1, row * 16 + 1)
         if row == 1:
@@ -93,31 +102,24 @@ for row in range(0, 4): #行數
             brick = Brick((65,105,225), column * 40 + 1, row * 16 + 1)
         if row == 3:
             brick = Brick((100,149,237), column * 40 + 1, row * 16 + 1)
-        bricks.add(brick)
+        bricks.add(brick) 
         allsprite.add(brick)
-message_start = "按滑鼠左鍵開始遊戲!!!"
-playing = False
-Running = True
-while Running: #遊戲基礎設定
-    clock.tick(50) #遊戲幀率
+msgstr = "按滑鼠左鍵開始遊戲！"  #起始訊息
+playing = False  #開始時球不會移動
+running = True
+while running:
+    clock.tick(30)
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: #關閉視窗
-            Running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not playing: #偵測遊戲是否開始
-            playing = True
-    if not playing: #遊戲起始介面
-        message = font.render(message_start, 1, (255,0,255))
-        screen.blit(message, (screen.get_width()/2-100, screen.get_height()/2))
-        pygame.display.update()
-    else: #遊戲中
-        #更新物件
-        ball_1.update()
-        pad.update()
-        bricks.update()
-        allsprite.draw(screen)
-        pygame.display.update()
-    screen.blit(background, (0, 0))
-    allsprite.draw(screen)
-    pygame.display.flip()
+        if event.type == pygame.QUIT:
+            running = False
+    buttons = pygame.mouse.get_pressed()
+    if buttons[0]:  #按滑鼠左鍵後球可移動
+        playing = True
+    if playing == True:  #遊戲中
+        screen.blit(background, (0,0))  #清除繪圖視窗
+        fail = ball.update()  #移動球體
+        allsprite.draw(screen)  #繪製所有角色
+    msg = font.render(msgstr, 1, (255,0,255))
+    screen.blit(msg, (screen.get_width()/2-60,screen.get_height()-20))  #繪製訊息
     pygame.display.update()
 pygame.quit()
